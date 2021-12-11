e6a015e52a40abf45638112beee487b442eef702
pokacyb <pokacyb@tutanota.com>
2021-12-11 19:21:51 +0100

    Play with routing

--- main.go
+++ main.go
@@ -15,9 +15,13 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:pokacyb@tutanota.com\">pokacyb@tutanota.com</a>.")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, r.URL.Path)
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
