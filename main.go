1b2261a3c9d9e9df183b65d589f0c10dc3b4bc1c
pokacyb <pokacyb@tutanota.com>
2022-01-11 19:58:34 +0100

    Comment out router struct

--- main.go
+++ main.go
@@ -15,35 +15,34 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:pokacyb@tutanota.com\">pokacyb@tutanota.com</a>.")
 }
 
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		// one liner version
-// 		http.Error(w, "Page not found", http.StatusNotFound)
-// 		// w.WriteHeader(http.StatusNotFound)
-// 		// fmt.Fprint(w, "Page not found")
-// 	}
-// }
-
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func pathHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
 	default:
+		// one liner version
 		http.Error(w, "Page not found", http.StatusNotFound)
+		// w.WriteHeader(http.StatusNotFound)
+		// fmt.Fprint(w, "Page not found")
 	}
 }
 
+// type Router struct{}
+
+// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, "Page not found", http.StatusNotFound)
+// 	}
+// }
+
 func main() {
-	var router Router
 	fmt.Println("Starting the server on :8000...")
-	http.ListenAndServe(":8000", router)
+	http.ListenAndServe(":8000", http.HandlerFunc(pathHandler)) // not a function call
 }
